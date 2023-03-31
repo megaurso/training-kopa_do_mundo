01270c092180be457440c842e7a071599b0394e2
megaurso <bernadogui@hotmail.com>
2023-03-31 14:01:22 -0300

    feat: finish task 3

--- teams/urls.py
+++ teams/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TeamView
+from .views import TeamView, TeamInfoView
 
 urlpatterns = [
     path("teams/", TeamView.as_view()),
+    path("teams/<int:team_id>/", TeamInfoView.as_view()),
 ]
